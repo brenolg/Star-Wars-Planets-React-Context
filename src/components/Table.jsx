ba7a41be53133c91fe0e385e09c2ed795b31775a
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-11-23 17:57:04 -0300

    2 - Criação de um filtro de texto para a tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,51 +1,74 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 export default function Table() {
   const { data } = useContext(StarWarsContext);
+  const [searchName, setSearchName] = useState('');
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
+    <>
+      <h1>Star Wars</h1>
 
-      <tbody>
+      <form>
+        <label htmlFor="name-filter">
+          <input
+            data-testid="name-filter"
+            name="name-filter"
+            type="text"
+            placeholder="Search by name"
+            onChange={ ({ target }) => setSearchName(target.value) }
+            value={ searchName }
+          />
+        </label>
+      </form>
 
-        {data.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>
-              {planet.films.map((film, index) => <p key={ index }>{film}</p>)}
-            </td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+
+          {data
+            .filter((line) => (
+              line.name.toLowerCase().includes(searchName.toLowerCase())
+            ))
+
+            .map((planet) => (
+              <tr key={ planet.name }>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>
+                  {planet.films.map((film, index) => <p key={ index }>{film}</p>)}
+                </td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
   );
 }
